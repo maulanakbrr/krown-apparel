270e9e847283220eb58b9467d58c7489b03b9145
Akbar Maulana <amaulanaa995@gmail.com>
2020-11-10 20:29:24 +0700

    added collection page and created selectCollection and selectCollectionsForPreview, and also implemented data normalization

--- src/components/collection-overview/collection-overview.jsx
+++ src/components/collection-overview/collection-overview.jsx
@@ -3,22 +3,25 @@ import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../collection-preview/collection-preview' ;
 
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collection-overview.scss';
 
-const CollectionOverview = ({collections}) => (
-    <div className="collection-overview">
-        {
-            collections.map( ({id, ...otherCollectionsProps}) => (
-                <CollectionPreview key={id} {...otherCollectionsProps}/>
-            ))
-        }
-    </div>
-);
+const CollectionOverview = ({collections}) => {
+    console.log(collections);
+    return(
+        <div className="collection-overview">
+            {
+                collections.map( ({id, ...otherCollectionsProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionsProps}/>
+                ))
+            }
+        </div>
+    )
+};
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,12 +1,15 @@
-import {Fragment} from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collection-overview/collection-overview';
+import CollectionPage from '../collection/collection';
 
-const ShopPage = () => {
+const ShopPage = ({match}) => {
+    
     return (
-        <Fragment>
-            <CollectionOverview/>
-        </Fragment>
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionOverview}/>
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+        </div>
     );
 }
 
